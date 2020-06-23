f17c4df7c55c5e19a5e9dc5b7282ab20899983da
Ryang Sohn <loop.infinitely@gmail.com>
2020-06-23 09:16:18 +0900

    Add collision finding code

---
+++ block-cipher/collisions.cc
@@ -0,0 +1,37 @@
+/*
+ * collisions.cc
+ * Code for finding collisions
+ */
+
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+#include "mininglib.h"
+
+int main() {
+  std::ifstream cipher_file("./ciphertext.txt");
+  cipher_file >> std::hex;
+  int cipher_byte;
+  std::vector<unsigned char> cipher;
+  while (cipher_file >> cipher_byte) cipher.push_back(cipher_byte);
+  cipher_file.close();
+
+  std::ifstream iv_file("./iv.txt");
+  iv_file >> std::hex;
+  int iv_byte;
+  std::vector<unsigned char> iv;
+  while (iv_file >> iv_byte) iv.push_back(iv_byte);
+  iv_file.close();
+
+  for (int i = 0; i < cipher.size(); ++i) {
+    if (i != 0 && i % 16 == 0) std::cout << std::endl;
+    std::vector<int> mined = mine_byte(iv, cipher, i / 16, i % 16);
+    std::cout << mined.size() << " ";
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
